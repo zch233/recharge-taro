ae72550008645c833f3f3abc594a91149fe397dc
zhangchao <529743595@qq.com>
2020-05-22 17:09:54 +0800

    Fix Coutry Count

--- src/pages/index/components/OverHeader/index.jsx
+++ src/pages/index/components/OverHeader/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export default function OverHeader ()  {
   return (
     <View className="overHeader">
-      <Text>现已开通10个国家手机充值服务</Text>
+      <Text>现已开通200+个国家手机充值服务</Text>
     </View>
   )
 }
